59b71d67d38177596cded2858c43f21a9f36ab3f
Ol4ik88 <slapikol4ik@gmail.com>
2022-08-16 20:21:53 +0300

    RSL-9: implement head and footer components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import guy from '../../assets/img/guy.svg';
+import userImg from '../../assets/img/user.svg';
+import logo from '../../assets/img/logo.svg';
+import './header.scss';
+
+class Header extends React.PureComponent {
+  render() {
+    return (
+      <div className="wrap wrap-header">
+        <header className="header">
+          <a href="/" className="logo">
+            <img src={logo} alt="" />
+          </a>
+          <div className="header__welcome">
+            <h1>Hello!</h1>
+            <p>It’s good to see you again.</p>
+          </div>
+          <img src={guy} className="header__img" alt="" />
+          <div className="logo-user">
+            <img src={userImg} alt="" />
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
