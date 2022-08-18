044fc8015b9b69053f028eace20e752feac48832
veronicavoevodina <veranika1711@gmail.com>
2022-08-18 10:46:32 +0300

    RSL-10: update project structure

--- src/components/shared/SidebarItem.tsx
+++ src/components/shared/SidebarItem.tsx
@@ -4,13 +4,15 @@ import { SidebarItemProps } from '../../types/index';
 
 class SidebarItem extends React.PureComponent<SidebarItemProps> {
   render(): React.ReactNode {
-    const { el } = this.props;
+    const {
+      el: { img, alt, name },
+    } = this.props;
     return (
       <li>
         <div className="img-item aside-cell">
-          <img src={el.img} alt={el.alt} />
+          <img src={img} alt={alt} />
         </div>
-        <div className="link-list-name aside-cell">{el.name}</div>
+        <div className="link-list-name aside-cell">{name}</div>
       </li>
     );
   }
