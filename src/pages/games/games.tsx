e0351dc406adaa5a2dc78542026ff8acee9be0ae
veronicavoevodina <veranika1711@gmail.com>
2022-08-19 15:27:05 +0300

    RSL-25: add game page structure

---
+++ src/pages/games/games.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+// import Sidebar from '../../components/shared/Sidebar';
+// import { wordApi } from '../../services/api/Words';
+
+function games() {
+  //  console.log(wordApi);
+  return <p>Audiocall</p>;
+}
+games();
