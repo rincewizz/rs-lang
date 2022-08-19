ca1403725d793ddad304522c4e32b56ddd22caf5
rincewizz <95530503+rincewizz@users.noreply.github.com>
2022-08-19 12:56:38 +0300

    Feature/rsl 20 create words api service (#8)
    * RSL-20: Create functions for words api
    
    * RSL-20: replace fetch with axios

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,6 +7,23 @@ export type SidebarItemProps = {
   };
 };
 
+export type Word = {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+};
+
 export interface ICreateUser {
   name: string;
   email: string;
