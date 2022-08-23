8ea3fb4d31ee6fcabbf2d05e348d243c623dc8c5
rincewizz <95530503+rincewizz@users.noreply.github.com>
2022-08-23 12:46:59 +0300

    RSL-34: add global state for user (#18)
    * RSL-35: Add zustand to package.json and package-lock.json
    
    * RSL-34: add global state for user
    
    * RSL-34: move auth store to new file

---
+++ src/services/storage/Auth.ts
@@ -0,0 +1,19 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import { IAuthStore } from '../../types';
+
+const useAuthStore = create<IAuthStore>()(
+  persist(
+    (set, get) => ({
+      auth: {},
+      setAuth: (auth) => set(() => ({ auth })),
+      getAuth: () => get().auth,
+    }),
+    {
+      name: 'auth-storage',
+      getStorage: () => localStorage,
+    }
+  )
+);
+
+export default { useAuthStore };
