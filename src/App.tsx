3700cf7fbd2e945583336ed333f6630bce56237b
veronicavoevodina <veranika1711@gmail.com>
2022-08-24 16:12:43 +0300

    RSL-25: fix mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import VoiceGameRound from './pages/voiceGame/VoicegameRound';
-/* import Home from './pages/home/Home';
- */
+import Home from './pages/home/Home';
 
 export default function App() {
-  return <VoiceGameRound />;
-  /*   return <Home />;
-   */
+  return <Home />;
 }
