881b70258b8a39b9f784d7b90ede7718f2134e74
Ol4ik88 <slapikol4ik@gmail.com>
2022-08-25 17:37:56 +0300

    RSL-37: Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/shared/Sidebar';
 import Home from './pages/home/Home';
+import Textbook from './pages/textbook/Textbook';
+import VoiceGame from './pages/voiceGame/Voicegame';
+import VoiceGameRound from './pages/voiceGame/VoicegameRound';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book" element={<Textbook />} />
+        <Route path="/voicegame" element={<VoiceGame />} />
+        <Route path="/voicegameround" element={<VoiceGameRound />} />
+        <Route path="*" element={<h2 className="textError">Ресурс не найден</h2>} />
+      </Routes>
+    </Router>
+  );
 }
