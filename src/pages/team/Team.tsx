805fd6fdc92bbd6af86500224481bdabc477cb0b
Ol4ik88 <99606360+Ol4ik88@users.noreply.github.com>
2022-08-26 17:39:28 +0300

    RSL-43: Create team page (#28)

---
+++ src/pages/team/Team.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TeamSection from '../../components/feature/TeamSection';
+import Footer from '../../components/shared/Footer';
+import Header from '../../components/shared/Header';
+
+export default function Team() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <TeamSection />
+      <Footer />
+    </div>
+  );
+}
