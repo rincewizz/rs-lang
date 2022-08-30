76df29367c18249b5e1ed4a7a3858c8f5bb78bb3
rincewizz <95530503+rincewizz@users.noreply.github.com>
2022-08-30 16:26:35 +0300

    RSL-53: add games statistic for textbook (#41)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-restricted-syntax */
 import { userStatisticApi } from '../services/api/Statistic';
-import { IStatisticGame } from '../types';
+import { usersWordsApi } from '../services/api/UsersWords';
+import {
+  IAuth,
+  IGameResults,
+  IStatisticGame,
+  IUserWordStaticstic,
+  UserWordOptions,
+  Word,
+} from '../types';
 
 export function getLengthCorrect(answers: boolean[]) {
   const chain = [];
@@ -71,4 +81,85 @@ export async function updateStaticGame(
     userId,
     request,
   });
+}
+
+class UserWordRequest {
+  request: UserWordOptions;
+
+  constructor(word: Word) {
+    this.request = word.userWord ?? { difficulty: 'none' };
+    if (!this.request.optional) {
+      this.request.optional = UserWordRequest.getNewOptional();
+    }
+    if (!this.request.optional.gamesStatistic) {
+      this.request.optional.gamesStatistic = UserWordRequest.getNewGamesStatistic();
+    }
+  }
+
+  static getNewOptional() {
+    return {
+      learned: false,
+      gamesStatistic: UserWordRequest.getNewGamesStatistic(),
+    };
+  }
+
+  static getNewGamesStatistic() {
+    return {
+      sprint: {
+        correct: 0,
+        incorrect: 0,
+      },
+      voice: {
+        correct: 0,
+        incorrect: 0,
+      },
+    };
+  }
+
+  set difficulty(val: string) {
+    this.request.difficulty = val;
+  }
+
+  set learned(val: boolean) {
+    if (this.request.optional) this.request.optional.learned = val;
+  }
+
+  set new(val: boolean) {
+    if (this.request.optional) this.request.optional.new = val;
+  }
+
+  private setGamesStatistic(game: 'sprint' | 'voice', val: IUserWordStaticstic) {
+    if (this.request?.optional?.gamesStatistic) {
+      this.request.optional.gamesStatistic[game].correct += val.correct;
+      this.request.optional.gamesStatistic[game].incorrect += val.incorrect;
+    }
+  }
+
+  set sprintGameStatistic(val: IUserWordStaticstic) {
+    this.setGamesStatistic('sprint', val);
+  }
+
+  set voiceGameStatistic(val: IUserWordStaticstic) {
+    this.setGamesStatistic('voice', val);
+  }
+}
+export async function recordWordsStatics(
+  auth: Partial<IAuth>,
+  game: 'sprint' | 'voice',
+  gameResults: IGameResults
+) {
+  if (!auth.token || !auth.userId) return;
+  for await (const [word, answer] of gameResults.entries()) {
+    if (!word._id) return;
+    const userWordRequest = new UserWordRequest(word);
+    userWordRequest[`${game}GameStatistic`] = answer;
+    const apiParam = {
+      token: auth.token,
+      userId: auth.userId,
+      wordId: word._id,
+      request: userWordRequest.request,
+    };
+    if (word.userWord) await usersWordsApi.updateUserWord(apiParam);
+    else await usersWordsApi.createUserWord(apiParam);
+  }
 }
